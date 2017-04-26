1f24a121717b3b988f72dc6486cf78b9357f6c8b
SleepingKnight <xSora48x@aim.com>
2017-04-26 15:12:17 -0500

    Pathfinding_Step

---
+++ Pathfinding.java
@@ -0,0 +1,22 @@
+/* This is to test how the final works with github
+* I will write out the steps to explain to a kid how to do it
+* With this I hope I can translate into code while testing
+* how github works.
+*
+*
+* First we need to input our data, for a test of the code we can use a small triangle as opposed to a super large one
+* My intial thought is to either put the data in a double array (arr[][]) or into a single array in sets: (arr[] = {1}, {2,3}, {4,5,6} )
+* From there we're going to need 
+
+public class Pathfinding
+{
+  
+
+
+
+
+
+
+
+
+}
